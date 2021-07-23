1ef67639441d9d77d3d9ede17798a018dbe0a26a
Matthew Treboschi <m3boschi@gmail.com>
2021-07-23 13:50:40 -0400

    home header change

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,9 +1,8 @@
 <?php
-require_once(__DIR__ . "/../../lib/functions.php");
+require_once(__DIR__ . "/../../partials/nav.php");
 if (!is_logged_in()) {
     die(header("Location: login.php"));
 }
-require_once(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <h5>Welcome, <?php se(get_username()); ?>!</h5>

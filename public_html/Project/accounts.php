dad5566b086098bce330110b6d99c654ec81b3a4
Matthew Treboschi <m3boschi@gmail.com>
2021-07-26 18:36:02 -0400

    fixed a spare script tag

--- public_html/Project/accounts.php
+++ public_html/Project/accounts.php
@@ -36,7 +36,7 @@ if (isset($_POST["submit"])) {
         <?php endforeach; ?>
     </table>
 </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
     function pst(accNum) {
         $.post({"accNum": accNum})
     }

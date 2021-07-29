f5fd68e736b76fc8c754364389d0733be3f2c50a
Matthew Treboschi <m3boschi@gmail.com>
2021-07-29 19:50:48 -0400

    fixed prev and next buttons working inappropriately

--- partials/pagination.php
+++ partials/pagination.php
@@ -8,16 +8,20 @@ if (!isset($total_pages)) {
     $total_pages = 1;
 } ?>
 <ul class="pagination">
-    <li class="page-item <?php if (($page - 1) < 1) echo 'disabled'; ?>">
+    <?php if (!(($page - 1) < 1)){ ?>
+    <li class="page-item">
         <a class="page-link" href="?<?php pagination_filter($page - 1); ?>">Previous</a>
     </li>
+    <?php } ?>
     <?php for ($i = 1; $i <= $total_pages; $i++) : ?>
         <li class="page-item <?php if ($page == $i) echo 'active'; ?>">
             <a class="page-link" href="?<?php pagination_filter($i); ?>">
                 <?php se($i); ?></a>
         </li>
     <?php endfor; ?>
-    <li class="page-item <?php if (($page + 1) > $total_pages) echo 'disabled'; ?>">
+    <?php if (!(($page + 1) > $total_pages)){ ?>
+    <li class="page-item">
         <a class="page-link" href="?<?php pagination_filter($page + 1); ?>">Next</a>
     </li>
+    <?php } ?>
 </ul>

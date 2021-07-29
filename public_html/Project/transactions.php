5ff3e6218ac8acc27cfd95220661719b28be5f54
Matthew Treboschi <m3boschi@gmail.com>
2021-07-29 19:58:44 -0400

    next pagination button debug

--- public_html/Project/transactions.php
+++ public_html/Project/transactions.php
@@ -20,7 +20,7 @@ if (isset($_GET["submit"])) {
     
 }
 $transactions = get_transactions($accNum, $start, $end, $type, $page);
-$total_pages = ceil(count($transactions)/10);
+$total_pages = floor(count($transactions)/10)+1;
 ?>
 <h1>This is the transactions page for account <?php echo($accNum)?></h1>
 <h3>Account Number: <?php se($info["account_number"]); ?></h3>

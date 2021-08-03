e7f14115ab089b3bd8118d84f23d07f53a933191
Matthew Treboschi <m3boschi@gmail.com>
2021-08-02 21:39:17 -0400

    attempt 2 at style

--- partials/nav.php
+++ partials/nav.php
@@ -15,8 +15,11 @@ session_set_cookie_params([
 ]);
 session_start();
 require_once(__DIR__ . "/../lib/functions.php");
-require_once(__DIR__ . "/../partials/style.css")
+
 ?>
+<style>
+<?php require_once(__DIR__ . "/../partials/style.css"); ?>
+</style>
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
